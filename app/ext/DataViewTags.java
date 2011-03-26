197cef86b480a29c4571efc4b77dfead885919b1
Julien Richard-Foy <julien@richard-foy.fr>
2011-03-26 13:57:41 +0100

    Small fix in textile file

--- app/ext/DataViewTags.java
+++ app/ext/DataViewTags.java
@@ -1,36 +1,34 @@
 package ext;
 
 import groovy.lang.Closure;
-
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
-
 import play.exceptions.TagInternalException;
 import play.exceptions.TemplateExecutionException;
 import play.templates.FastTags;
 import play.templates.GroovyTemplate.ExecutableTemplate;
 import play.templates.TagContext;
 
+import java.io.PrintWriter;
+import java.util.Map;
+
 public class DataViewTags extends FastTags {
 
-	public static void _table(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
-		Iterable<?> data = (Iterable<?>)args.get("arg");
-		if (data == null) {
-			throw new TemplateExecutionException(template.template, fromLine, "Please specify the data to display", new TagInternalException("Please specifiy the data to display"));
-		}
-		String it = (String)args.get("as");
-		if (it == null) {
-			throw new TemplateExecutionException(template.template, fromLine, "Missing parameter 'as'", new TagInternalException("Missing parameter 'as'"));
-		}
-		
-		if (!args.containsKey("class")) {
-			out.println("<table>");
-		} else {
-			out.println("<table class=\"" + args.get("class") + "\">");
-		}
+    public static void _table(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        Iterable<?> data = (Iterable<?>) args.get("arg");
+        if (data == null) {
+            throw new TemplateExecutionException(template.template, fromLine, "Please specify the data to display", new TagInternalException("Please specifiy the data to display"));
+        }
+        String it = (String) args.get("as");
+        if (it == null) {
+            throw new TemplateExecutionException(template.template, fromLine, "Missing parameter 'as'", new TagInternalException("Missing parameter 'as'"));
+        }
 
-		String rowClass = args.containsKey("rowClass") ? (String)args.get("rowClass") : "";
+        if (!args.containsKey("class")) {
+            out.println("<table>");
+        } else {
+            out.println("<table class=\"" + args.get("class") + "\">");
+        }
+
+        String rowClass = args.containsKey("rowClass") ? (String) args.get("rowClass") : "";
 
         if (body == null) {
             // Automatically fill the table content with the models properties
@@ -38,51 +36,51 @@ public class DataViewTags extends FastTags {
             // Fill the table content with the execution of its body
 
             // A first time for the header row
-			TagContext.current().data.put("dataview.state", "head");
-			if (rowClass.isEmpty()) {
-				out.println("<tr>");
-			} else {
-				out.println("<tr class=\"" + rowClass + "\">");
-			}
-			body.call();
-			out.println("</tr>");
+            TagContext.current().data.put("dataview.state", "head");
+            if (rowClass.isEmpty()) {
+                out.println("<tr>");
+            } else {
+                out.println("<tr class=\"" + rowClass + "\">");
+            }
+            body.call();
+            out.println("</tr>");
 
             // Then for each row
-			TagContext.current().data.put("dataview.state", "content");
-			int index = 0;
-			for (Object row : data) {
-				String parity = (index % 2) == 0 ? " even" : " odd";
-				out.println("<tr class=\"" + rowClass + parity + "\">");
-				
-				body.setProperty(it, row);
-				body.call();
-				
-				out.println("</tr>");
-				
-				index++;
-			}
-		}
-		out.println("</table>");
-	}
-	
-	public static void _column(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
-		String state = (String)TagContext.parent("table").data.get("dataview.state");
-		if (state.equals("head")) {
-			out.print("<th>");
-			if (args.containsKey("arg")) {
-				out.print(args.get("arg"));
-			}
-			out.println("</th>");
-		} else {
-			if (!args.containsKey("class")) {
-				out.print("<td>");
-			} else {
-				out.print("<td class=\"" + args.get("class") + "\">");
-			}
-			if (body != null) {
-				body.call();
-			}
-			out.println("</td>");
-		}
-	}
+            TagContext.current().data.put("dataview.state", "content");
+            int index = 0;
+            for (Object row : data) {
+                String parity = (index % 2) == 0 ? " even" : " odd";
+                out.println("<tr class=\"" + rowClass + parity + "\">");
+
+                body.setProperty(it, row);
+                body.call();
+
+                out.println("</tr>");
+
+                index++;
+            }
+        }
+        out.println("</table>");
+    }
+
+    public static void _column(Map<?, ?> args, Closure body, PrintWriter out, ExecutableTemplate template, int fromLine) {
+        String state = (String) TagContext.parent("table").data.get("dataview.state");
+        if (state.equals("head")) {
+            out.print("<th>");
+            if (args.containsKey("arg")) {
+                out.print(args.get("arg"));
+            }
+            out.println("</th>");
+        } else {
+            if (!args.containsKey("class")) {
+                out.print("<td>");
+            } else {
+                out.print("<td class=\"" + args.get("class") + "\">");
+            }
+            if (body != null) {
+                body.call();
+            }
+            out.println("</td>");
+        }
+    }
 }
